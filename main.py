90e3df154c350cf6fc6fab9836e2adb466bd5197
gedizzletsplay@gmail.com <Duke1@234>
2022-05-04 14:22:00 +0100

    flush for further editing.

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ html = """
 <html>
 <title>Mildenhall MAC</title>
 <link rel="icon" type="image/x-icon" href="favicon.ico">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, user_scalable=no, target-densitydpi=low-dpi"/>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -79,6 +79,10 @@ html = """
 table {
   border:1px solid black;
   font-size: 1vw
+}
+
+.row {
+width: 100%
 }
 
 .specialtable {
